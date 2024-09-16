73af1a1f217557bafc934b08a8196e2fe327773b
JoaoZ14 <csgojoao84@gmail.com>
2024-09-16 20:39:33 -0300

    att

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -33,10 +33,13 @@ const DivIcons = styled.div`
 
 const Wraper = styled.div`
   background-color: #212121;
+  display: flex; // usa flexbox para criar um layout flexível
+  justify-content: center; // centraliza o conteúdo horizontalmente
+  align-items: center; // centraliza o conteúdo verticalmente
 `;
 
 const DivPrincipal = styled.div`
-  margin-top: 10%;
+  margin-top: 100px;
   text-align: center;
   align-items: center;
   display: flex;
@@ -45,24 +48,35 @@ const DivPrincipal = styled.div`
 `;
 
 const DivText = styled.div`
-  width: 1100px;
+  width: 1200px;
   text-align: center;
   flex-direction: column;
 `;
 
 const TextFirst = styled.h1`
   font-family: Garamond, serif;
-  font-size: 100px;
+  font-size: 100px; // define o tamanho da fonte como 10% do viewport
   font-weight: 100;
   margin: 0;
 `;
+
 const TextSecond = styled.h2`
   margin-top: 0;
   font-family: Garamond, serif;
-  font-size: 50px;
+  font-size: 50px; // define o tamanho da fonte como 5% do viewport
   font-weight: 100;
 `;
-
+const TextSecondDigit = styled.h2`
+  margin-top: -40px;
+  font-family: Garamond, serif;
+  font-size: 30px; // define o tamanho da fonte como 5% do viewport
+  font-weight: 100;
+  letter-spacing: 5px;
+  text-transform: uppercase;
+  display: flex;
+  text-align: center;
+  justify-content: center;
+`;
 const ButtonCV = styled.a`
   background-color: transparent;
   padding: 5px;
@@ -86,15 +100,41 @@ const Line = styled.div`
   background-color: silver;
 `;
 
+const AboutDiv = styled.div`
+  position: relative;
+  width: 1000px;
+  height: 200px;
+`;
+
+const AboutText = styled.p`
+  text-align: center; /* Centraliza o texto */
+  font-size: 25px;
+  z-index: 11;
+  margin-top: -100px;
+`;
+
+const AboutTitle = styled.h1`
+  margin-top: -20px;
+  text-align: center; /* Centraliza o texto */
+  font-family: "Playfair Display", serif;
+  font-optical-sizing: auto;
+  font-size: 300px;
+  color: rgba(100, 100, 100, 0.5);
+  font-style: normal;
+  font-weight: 200;
+  position: relative;
+  width: 100%;
+  height: 0px;
+`;
+
 const LandingPage = () => {
   return (
     <Wraper>
       <DivPrincipal>
         <DivText>
           <TextFirst>Hello World,</TextFirst>
-          <TextSecond>
-            Meu nome é João Guilherme, e sou desenvolvedor front-end - React
-          </TextSecond>
+          <TextSecond>Meu nome é João Guilherme,</TextSecond>
+          <TextSecondDigit>desenvolvedor frontend</TextSecondDigit>
           <ButtonCV href="">Baixar CV</ButtonCV>
         </DivText>
         <DivIcons>
@@ -111,6 +151,19 @@ const LandingPage = () => {
           </a>
         </DivIcons>
         <Line />
+        <AboutDiv>
+          <AboutTitle>SOBRE</AboutTitle>
+          <AboutText>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+            sit amet nisl rhoncus, commodo est ut, lobortis lectus. Cras
+            porttitor tempor urna, sit amet convallis nisl eleifend eu. Integer
+            consectetur lacus sed nibh maximus, in condimentum lorem cursus.
+            Phasellus vel vestibulum tortor. Pellentesque non arcu volutpat,
+            placerat metus sed, commodo purus.
+          </AboutText>
+        </AboutDiv>
+        <Line />
+
         <Line />
       </DivPrincipal>
     </Wraper>
